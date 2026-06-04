b7d419e9d00fb4c4ab39ca600d5d411b92146629
codingradhe <rc842944@gmail.com>
2026-06-04 09:29:12 +0530

    Add makeNode solution

---
+++ 001.makeNode.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class node{
+	public:
+	int data;
+	node* next;
+	public:
+
+	node(int data1,node* next1){
+		data= data1,next = next1;
+	}
+};
+
+int main()
+{
+	//write  your code here;
+}
