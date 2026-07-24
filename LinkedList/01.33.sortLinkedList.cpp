fa3cc7254cce117b2e6431989a0b5d0f7ecb4337
codingradhe <rc842944@gmail.com>
2026-07-24 09:00:03 +0530

    Implement sort function in linked list brute and merge sort
    Refactor merge sort implementation for linked list. Update findMid and merge functions for improved clarity and correctness.

--- LinkedList/01.33.sortLinkedList.cpp
+++ LinkedList/01.33.sortLinkedList.cpp
@@ -51,3 +51,67 @@ public:
     }
 };
 //  apply merge sort in linked list
+// ❤️❤️❤️❤️ modifind hare and tortoise algorithm 
+// slow = head;; BUT 
+// fast = head->next; to find exact middle in any case ;
+class Solution {
+public:
+    ListNode* findMid(ListNode* head){
+        if(!head||!head->next) return head;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast&&fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* merge(ListNode* heada,ListNode*headb){
+        if(heada==nullptr && headb ==nullptr) return nullptr;
+        if(heada==nullptr) return headb;
+        if(headb==nullptr) return heada;
+        ListNode* newhead;
+        ListNode* temp1 = heada;
+        ListNode* temp2 = headb;
+        if(temp1->val>=temp2->val){
+            newhead = temp2;
+            temp2 = temp2->next;
+        }
+        else{
+            newhead = temp1;
+            temp1= temp1->next;
+        }
+        ListNode* mover = newhead;
+        while(temp1!= nullptr&&temp2!=nullptr){
+            if(temp1->val>=temp2->val){
+                mover->next= temp2;
+                temp2 = temp2->next;
+                mover= mover->next;
+            }
+            else if(temp1->val<temp2->val){
+                mover->next= temp1;
+                temp1 = temp1->next;
+                mover= mover->next;
+            }
+        }
+        if(temp1) mover->next = temp1;
+        if(temp2) mover->next = temp2;
+        return newhead;
+    }
+    ListNode* sortList(ListNode* head){
+        if(!head || !head->next) return head;
+        ListNode* mid = findMid(head);
+        ListNode* head2 = mid->next;
+        mid->next = nullptr;
+        ListNode* heada = sortList(head);
+        ListNode* headb = sortList(head2);
+        return merge(heada,headb);
+    }
+};
+
+
+
+
+
+
+
