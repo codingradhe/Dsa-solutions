bbab4b9003843db25d43a2aee7559e9d37e84abc
Radheshyam chaudhary <rc842944@gmail.com>
2026-09-14 11:36:27 +0530

    Add solution for kth largest element in an array
    Implement a solution to find the kth largest element in an array using a priority queue.

---
+++ Heap/09.kthLargestElement.cpp
@@ -0,0 +1,25 @@
+// 215. Kth Largest Element in an Array
+// Medium
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// Can you solve it without sorting?
+class Solution {
+public:
+    int findKthLargest(vector<int>& v, int k) {
+        priority_queue<int>q;
+        int  n = v.size();
+        for(int i = 0;i < n ;i++){
+            q.push(v[i]);
+        }
+        int cnt = 0;
+        while(!q.empty()){
+            k--;
+            int x = q.top();
+            if(k == 0) return x;
+            q.pop();
+        }
+        return 0;
+    }
+};
